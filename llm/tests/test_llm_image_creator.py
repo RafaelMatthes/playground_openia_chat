c259d49a50259c76db3aedb7ff22b0599e69ca4c
Rafael Matthes <42219515+RafaelMatthes@users.noreply.github.com>
2024-04-25 19:01:52 -0300

    Revert "Revert "Dall-E""

---
+++ llm/tests/test_llm_image_creator.py
@@ -0,0 +1,54 @@
+import pytest
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from dataclasses import dataclass
+
+
+@pytest.fixture
+def client():
+    from app import app
+    return TestClient(app)
+
+
+@dataclass
+class MockIAMessage:
+    content: str
+
+
+@dataclass
+class MockImagesResponse:
+    created: int
+    data: list
+    content: str
+
+
+@patch('llm.llm_image_creator.LlmImagesCreator.create_image')
+def test_create_images_endpoint(mock_dall_e, client):
+
+    mock_dall_e.return_value = MockImagesResponse(created=123, data=[], content="{'img': '123'}")
+
+    response = client.post('image/chat/create', json={"question": "Test question", "credentials": {"endpoint": "test_endpoint", "api_key": "test_api_key"}})
+    assert response.status_code == 200
+
+
+@patch('llm.llm_image_creator.LlmImagesCreator.create_image')
+@patch('langchain_openai.AzureChatOpenAI.invoke')
+def test_creative_chat_endpoint(mock_dall_e, mock_invoke, client):
+
+    mock_dall_e.return_value = MockImagesResponse(created=123, data=[], content="{'img': '123'}")
+    mock_invoke.return_value = MockImagesResponse(created=123, data=[], content="{'img': '123'}")
+
+    response = client.post(
+        'image/chat/creative_chat',
+        json={
+            "question": "Test question",
+            "credentials": {
+                "endpoint": "test_endpoint",
+                "api_key": "test_api_key"
+            },
+            "chat_history": [
+                {"role": "user", "content": "Test message"}
+            ]
+        }
+    )
+    assert response.status_code == 200
--- routes/dalle_route.py
+++ routes/dalle_route.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Body, HTTPException
-from models.chat_image_payload import ChatImagePayload
+from models.chat_image_payload import ChatImagePayload, ChatCreativePayload
 from openai.types import ImagesResponse
 
 from llm.llm_image_creator import (
     LlmImagesCreator, 
+    LlmCreativeImageCreator,
     ImageCreatorNotFoundError, 
     ImageCreatorPolicyError
 )
@@ -12,11 +13,27 @@ dall_e = APIRouter()
 
 
 @dall_e.post('/create', status_code=200)
-async def chat_whit_history(chat_payload: ChatImagePayload = Body(...)) -> ImagesResponse:
+async def create_images(chat_payload: ChatImagePayload = Body(...)) -> ImagesResponse:
     try:
         return LlmImagesCreator(
                 credentials=chat_payload.credentials
             ).create_image( 
+                question=chat_payload.question
+            )
+
+    except ImageCreatorNotFoundError as err:
+        raise HTTPException(status_code=404, detail=str(err))
+    except ImageCreatorPolicyError as err:
+        raise HTTPException(status_code=400, detail=str(err))
+
+
+@dall_e.post('/creative_chat', status_code=200)
+async def creative_chat(chat_payload: ChatCreativePayload = Body(...)) -> ImagesResponse:
+    try:
+        return LlmCreativeImageCreator(
+                credentials=chat_payload.credentials
+            ).creative_chat( 
+                chat_history=chat_payload.chat_history,
                 question=chat_payload.question
             )
 
